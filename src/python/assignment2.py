2837f9dd01e8103484d9ee23b8740ac5776f2fae
AIArka1 <arkapravabhattacharya312@gmail.com>
2026-02-13 12:40:21 +0530

    New chapter

---
+++ src/python/assignment2.py
@@ -0,0 +1,5 @@
+# Take the diameter as input and calculate the area of a circle
+
+diameterOfCircle = float(input("Enter the diameter of the circle : "))
+areaOfCircle = 3.14 * (diameterOfCircle/2) **2
+print("The area of the circle is", areaOfCircle)
