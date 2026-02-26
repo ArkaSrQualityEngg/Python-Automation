4d7f28570cb4b12945b3da6a024d3a2242aa6af2
AIArka1 <arkapravabhattacharya312@gmail.com>
2026-02-26 09:23:02 +0530

    Python Strings

---
+++ src/python/stringsInPython.py
@@ -0,0 +1,27 @@
+print("Hello"+"World")
+str = "Arka is a nice boy !!"
+print(len(str))
+print(str[5])
+print(str[:7])
+print(str[8:])
+print(str[3:9])
+print(str[:])
+print(str.find("is"))
+print(str.upper())
+str1 ="YOU ARE UGLY AF !!"
+print(str1.lower())
+print(str1.islower())
+print(str1.count("a"))
+print(str1.count("A"))
+str2 = "human centipede"
+print(str2.capitalize())
+str3 ="brave new world"
+print(str3.title())
+print(str3.replace("brave","Captain America Brave"))
+## Formatted strings
+name = "Arka"
+age = 30
+print(f"My name is {name} and age is {age}")
+print("Hello\nWorld\tfuckers")
+print('My birthday\'s on the 25th of June')
+print("He said, \"Fuck You !!\"")
