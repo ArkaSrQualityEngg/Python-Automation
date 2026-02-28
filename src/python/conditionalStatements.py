83c7a677dfd219fe83e5b85273e41a69ed00289a
AIArka1 <arkapravabhattacharya312@gmail.com>
2026-02-28 07:18:09 +0530

    Python conditional statements

---
+++ src/python/conditionalStatements.py
@@ -0,0 +1,10 @@
+# num = int(input("Enter a number :"))
+num = 9
+if num > 0:
+    print(num," is positive")
+
+elif num == 0:
+    print(num, " is equal to 0")
+
+else:
+    print(num," is negative")
